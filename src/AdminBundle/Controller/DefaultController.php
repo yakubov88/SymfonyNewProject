f11fe6e4883ed23388c1ba46a2f9b18ba3482803
yakubov88 <y.yakuboov@gmail.com>
2016-11-13 11:34:07 +0200

    input changes

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -97,6 +97,7 @@ class DefaultController extends Controller
 
                 return $this->redirect($this->generateUrl('admin'));
             }
+            
         catch (EntityNotFoundExceptionxception $ex )
             {
                 $ex->getMessage();
